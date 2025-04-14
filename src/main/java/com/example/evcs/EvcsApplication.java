28055447aad4985289586df926bf8234612fc748
LEKM <83527522+als229@users.noreply.github.com>
2025-04-14 15:31:55 +0900

    망가져라

--- src/main/java/com/example/evcs/EvcsApplication.java
+++ src/main/java/com/example/evcs/EvcsApplication.java
@@ -3,11 +3,29 @@ package com.example.evcs;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class EvcsApplication {
+import com.example.evcs.test.sorry.*;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@SpringBootApplication
+@RequiredArgsConstructor
+@Slf4j
+public class EvcsApplication {
+	
 	public static void main(String[] args) {
+		
+		Sorry sorry = new Sorry("멍멍멍멍멍", " 왈로왈왈왈", "미안하면 군생활 끝나나?");
+		
+		System.out.println(sorry.getMungmung() + sorry.getSorry());
+		System.out.println("너 뭐닝?" + sorry);
+		
+		log.info("이게 되네 {}" , sorry);
+		
+		
+		
 		SpringApplication.run(EvcsApplication.class, args);
+		
 	}
 
 }
--- src/main/java/com/example/evcs/test/Sorry.java
+++ src/main/java/com/example/evcs/test/Sorry.java
@@ -5,6 +5,8 @@ public class Sorry {
 	
 	{
 		System.out.print("ㅈㅅ합니당 ㅎㅎ");
+		System.out.println("ㅎㅎ 머지;");
+		System.out.println("하이 나는 엘리ㅃ먼뜨");
 	}
 
 }
