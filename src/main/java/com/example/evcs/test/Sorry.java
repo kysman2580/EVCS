741d637f88235ed84d6ff4e7868f513d6051afee
이지우 <127360928+jiwoolee2@users.noreply.github.com>
2025-04-14 15:50:49 +0900

    0202

--- src/main/java/com/example/evcs/test/Sorry.java
+++ src/main/java/com/example/evcs/test/Sorry.java
@@ -15,6 +15,7 @@ public class Sorry {
 		System.out.println("lelelelelele");
 		log.info("너네는 롬복 안 깔아서 안될걸??? 풉키 풉키");
 		log.info("지금 브랜치 새로 팠고 풀 받았습니다잉 ");
+		log.info("진짜 리얼로다가 꺠달아 버렸어요 어떻게 하는지 완벽하게 이해해 버렸다구요");
 	}
 	
 
