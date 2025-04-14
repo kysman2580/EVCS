821a208a2e42b0d535de3ce73cd7d71423617987
LEKM <83527522+als229@users.noreply.github.com>
2025-04-14 16:07:29 +0900

    boxing king

--- src/main/java/com/example/evcs/test/Sorry.java
+++ src/main/java/com/example/evcs/test/Sorry.java
@@ -1,19 +1,37 @@
 package com.example.evcs.test;
 
+import com.example.evcs.test.boxing.BoxingKing;
+
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
+@RequiredArgsConstructor
 public class Sorry {
+	
+	private final BoxingKing boxing;
+	private String mung;
+	
+	public Sorry (String mung) {
+	
+		this.boxing = new BoxingKing();
+		this.mung = mung;
+	}
 	
 	
 	{
 		System.out.print("ㅈㅅ합니당 ㅎㅎ");
-		System.out.println("ㅎㅎ 머지;");
-		System.out.println("하이 나는 엘리ㅃ먼뜨");
 		System.out.println("하이하이하이");
+		System.out.println("하이 나는 엘리ㅃ먼뜨");
+		System.out.println("히히 사이에 넣어버러기");
+		System.out.println("ㅎㅎ 머지;");
 		log.info("dfdsfsd");
 		
+		log.info("{}");
+
 		log.info("너네는 롬복 안 깔아서 안될걸??? 풉키 풉키");
+		
+		
 	}
 	
 
